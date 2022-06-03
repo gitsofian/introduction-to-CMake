0d079a4f562534a7e5773e3a52540f230ad12f98
sofiane <sofiane@meriane.dev>
2022-06-03 23:25:24 +0200

    Projekt aktualisiert

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,16 @@
 template <typename T>
 requires std::integral<T>
 
-T add(T a, T b)
+    T add(T a, T b)
 {
     return a + b;
-}
-
-T mul(T a, T b)
-{
-    return a*b;
 }
 
 int main()
 {
     std::cout << "Hello from C++ 2ß with CMake on Linux/window" << std::endl;
     std::cout << "The sum is : " << add(4, 4) << std::endl;
-    std::cout << "The multiplication is : " << mul(4, 4) << std::endl;
-cd
+    std::cout << "The multiplication is : " << add(4, 4) << std::endl;
 
     return EXIT_SUCCESS;
 }
