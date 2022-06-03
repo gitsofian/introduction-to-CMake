57d43b47f0f8e7f09afb2091943b1e77c90c8d85
sofiane <sofiane@meriane.dev>
2022-06-03 21:37:26 +0200

    Project aktualisiert

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+
+    T add(T a, T b)
+{
+    return a + b;
+}
+
+int main()
+{
+    std::cout << "Hello from C++ 2ß with CMake on Linux/window" << std::endl;
+    std::cout << "The sum is : " << add(4, 4) << std::endl;
+    return EXIT_SUCCESS;
+}
