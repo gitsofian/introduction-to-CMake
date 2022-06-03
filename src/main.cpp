be247ed113180d07bb5dfacfbaa6c26d0f2d89e6
Sofiane <sofiane@meriane.dev>
2022-06-03 22:52:18 +0200

    Mul() func. hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,22 @@
 template <typename T>
 requires std::integral<T>
 
-    T add(T a, T b)
+T add(T a, T b)
 {
     return a + b;
+}
+
+T mul(T a, T b)
+{
+    return a*b;
 }
 
 int main()
 {
     std::cout << "Hello from C++ 2ß with CMake on Linux/window" << std::endl;
     std::cout << "The sum is : " << add(4, 4) << std::endl;
+    std::cout << "The multiplication is : " << mul(4, 4) << std::endl;
+cd
+
     return EXIT_SUCCESS;
 }
